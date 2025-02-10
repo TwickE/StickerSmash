1ad0eeff0e34cf60d535736cd138b68c52ab76dd
TwickE <118013263+TwickE@users.noreply.github.com>
2025-02-10 17:19:13 +0000

    Added the functionality to export the images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,21 +2,32 @@ import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import { ImageSource } from "expo-image";
 import EmojiSticker from "@/components/EmojiSticker";
+import * as MediaLibrary from "expo-media-library";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("../../assets/images/background-image.png")
 
 export default function Index() {
-    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
-    const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
-    const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-    const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined)
+    const imageRef = useRef(null);
+    const [premissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+    const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+
+    useEffect(() => {
+        if (!premissionResponse?.granted) {
+            requestPermission();
+        }
+    }, []);
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -46,11 +57,25 @@ export default function Index() {
         setIsModalVisible(true);
     };
 
-    const onSaveImageAsync = async () => { };
+    const onSaveImageAsync = async () => {
+        try {
+            const localUri = await captureRef(imageRef, {
+                height: 440,
+                quality: 1,
+            });
+
+            await MediaLibrary.saveToLibraryAsync(localUri);
+            if (localUri) {
+                alert('Saved!');
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     return (
         <View style={styles.container}>
-            <View style={styles.imageContainer}>
+            <View ref={imageRef} collapsable={false}>
                 <ImageViewer imgSource={selectedImage || PlaceholderImage} />
                 {pickedEmoji && (
                     <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
