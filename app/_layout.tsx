b38d8d85c745b0b029b9e00b09cb097c6b9e47c2
TwickE <118013263+TwickE@users.noreply.github.com>
2025-02-10 16:56:16 +0000

    Added gestures to move and increase the sitcker size

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
     return (
-        <>
+        <GestureHandlerRootView>
         <StatusBar style="light" />
             <Stack>
                 <Stack.Screen name="(tabs)" options={{
@@ -11,6 +12,6 @@ export default function RootLayout() {
                 }} />
                 <Stack.Screen name="+not-found" options={{}} />
             </Stack>
-        </>
+        </GestureHandlerRootView>
     );
 }
