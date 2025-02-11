3152d1cd4b204a7d244ddaa2eb9f305d16c53e9c
TwickE <118013263+TwickE@users.noreply.github.com>
2025-02-11 17:22:45 +0000

    Added a splash screen and information in the about page

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,10 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>
+        This Project was made by{'\n'}
+        <Text style={styles.textBold}>Frederico Silva</Text>
+        {'\n'} following a Expo tutorial</Text>
     </View>
   );
 }
@@ -17,5 +20,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    fontSize: 20,
+    padding: 60,
+    textAlign: 'center',
   },
+  textBold: {
+    fontWeight: 'bold',
+  }
 });
